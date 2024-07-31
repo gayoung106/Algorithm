ab7875fda6eda568bd7fa5399fe60b0f6cd582ed
GAZERO <98731537+gayoung106@users.noreply.github.com>
2024-07-31 19:27:39 +0900

    [level 0] Title: 배열 조각하기, Time: 1.12 ms, Memory: 39 MB -BaekjoonHub

--- 프로그래머스/0/181893. 배열 조각하기/배열 조각하기.js
+++ 프로그래머스/0/181893. 배열 조각하기/배열 조각하기.js
@@ -1,14 +1,4 @@
 function solution(arr, query) {
-    for(let i = 0; i < query.length; i++) {
-        if(i % 2) {
-            // 홀수 인덱스 이전 제거
-            arr.splice(0, query[i])
-        } else {
-            // 짝수 인덱스 이후 제거
-            arr.splice(query[i] + 1)
-        }
-        
-    }
-    return arr
-
+    var answer = [];
+    return answer;
 }
